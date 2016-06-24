d1f64b6d65ee0b2343cfe4530704da86c8053786
mtmcduffie <mcduffie.michael@gmail.com>
2016-06-24 15:42:50 -0400

    DI-366 - This is run by start script now.

--- local_environment/transmart_db/customize_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker cp customize_install.sql i2b2transmartdb:/usr/local/customize_install.sql
-docker exec i2b2transmartdb /bin/sh -c "chmod 770 /usr/local/customize_install.sql"
-docker exec i2b2transmartdb /bin/sh -c "export ORACLE_SID=XE;export PATH=/u01/app/oracle/product/11.2.0/xe/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe;sqlplus system/oracle @/usr/local/customize_install.sql"
