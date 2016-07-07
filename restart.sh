5c60c4502036bd9bdb7b11806f5596e86f3db284
mtmcduffie <mcduffie.michael@gmail.com>
2016-07-07 10:01:10 -0400

    DI-410 - This doesn't belong here.

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker stop i2b2transmartapp
-docker rm i2b2transmartapp
-docker run -d -it --memory="2g" -p 8090:8090 --name i2b2transmartapp -e environment=tm-dev dbmi/i2b2transmart-app
