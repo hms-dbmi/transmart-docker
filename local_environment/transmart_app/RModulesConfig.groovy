2c0ff48d4912e752faf76010fb40821b0c1b96f3
Andre Rosa <andre_rosa@hms.harvard.edu>
2016-10-06 15:29:05 -0400

    DI-334
    1) configuration files are added after starting up container. Should
    not bake them into image. System-dependent.

---
+++ local_environment/transmart_app/RModulesConfig.groovy
@@ -0,0 +1,23 @@
+/**
+* This is the config file used by the RModules plugin
+* @author SMunikuntla
+*
+*/
+RModules 
+{
+	//If not pulling R Scripts from Github, this needs to be used to point to the local file system.
+    pluginScriptDirectory = "/Users/mmcduffie/src/tranSMART/Rmodules1_0/web-app/Rscripts/"
+    
+	defaultStatusList = ["Started",
+			"Validating Cohort Information",
+			"Triggering Data-Export Job",
+			"Gathering Data",
+			"Running Conversions",
+			"Running Analysis",
+			"Rendering Output"]
+	   
+	//Configuration for plugins.
+	tempFolderDirectory = "/mnt/tmp/jobs/"
+	   	   
+}
+
