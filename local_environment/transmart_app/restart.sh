f690ce5bb8eceb8d68588a71b894c7ac60cad450
Andre Rosa <andre_rosa@hms.harvard.edu>
2016-10-24 17:28:09 -0400

    DI-579 revert back
    revert back restart script to what it was before Andre had no idea what
    was going on and why, e.g. “Why aren’t you working! ok let’s
    temporarily copy these files so I can muck with them”
    
    all groovy files and server.xml was moved to the Dockerfile

--- local_environment/transmart_app/restart.sh
+++ local_environment/transmart_app/restart.sh
@@ -3,13 +3,3 @@
 docker stop i2b2transmartapp
 docker rm i2b2transmartapp
 docker run -d -it --memory="2g" -p 8090:8090 --net i2b2-net --name i2b2transmartapp --label app.name=transmart-1.2.4 --label app.environment=dev -e environment=tm-dev dbmi/i2b2transmart-app
-docker exec -it i2b2transmartapp /bin/sh -c "mkdir /root/.grails/"
-docker exec -it i2b2transmartapp /bin/sh -c "mkdir /root/.grails/transmartConfig/"
-docker cp DataSource.groovy i2b2transmartapp:/root/.grails/transmartConfig/DataSource.groovy
-docker cp Config.groovy i2b2transmartapp:/root/.grails/transmartConfig/Config.groovy
-docker cp BuildConfig.groovy i2b2transmartapp:/root/.grails/transmartConfig/BuildConfig.groovy
-docker cp RModulesConfig.groovy i2b2transmartapp:/root/.grails/transmartConfig/RModulesConfig.groovy
-#sleep 20
-docker cp server.xml i2b2transmartapp:/usr/local/tomcat/conf/server.xml
-#docker cp transmart.war.1.2.4 i2b2transmartapp:/usr/local/tomcat/webapps/transmart.war
-docker exec -it i2b2transmartapp /bin/sh -c './bin/catalina.sh run'
